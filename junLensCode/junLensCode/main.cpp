96678706ea728428b7aa307cbc2ba12aef4a4c8d
Jun Cheng <cheng109@purdue.edu>
2015-09-05 15:13:06 -0400

    initiate the project

---
+++ junLensCode/junLensCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  junLensCode
+//
+//  Created by cheng109 on 9/5/15.
+//  Copyright (c) 2015 cheng109. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
+
+
